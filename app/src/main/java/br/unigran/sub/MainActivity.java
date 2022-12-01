937e5bd9a0ade2ffe3ef56c6509dba87cf8271dd
Leonardo <802.293@alunos.unigran.br>
2022-12-01 20:41:08 -0300

    Comeco do firebase e tela grafica

--- app/src/main/java/br/unigran/sub/MainActivity.java
+++ app/src/main/java/br/unigran/sub/MainActivity.java
@@ -3,12 +3,18 @@ package br.unigran.sub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 }
